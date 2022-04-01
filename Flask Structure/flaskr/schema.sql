0f1d4a997ee81c4cc61d2bc950ae08ff17d1389e
Arthur De Neyer <arthur@arthur-linux>
2022-04-01 23:29:33 +0200

    created real flask files on which to work, changes to be made ofc

---
+++ Flask Structure/flaskr/schema.sql
@@ -0,0 +1,20 @@
+-- Comment: This is a temp database. We are waiting 
+-- for the real database about "la ferme des trois chênes"
+
+DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS post;
+
+CREATE TABLE user (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  username TEXT UNIQUE NOT NULL,
+  password TEXT NOT NULL
+);
+
+CREATE TABLE post (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  author_id INTEGER NOT NULL,
+  created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  title TEXT NOT NULL,
+  body TEXT NOT NULL,
+  FOREIGN KEY (author_id) REFERENCES user (id)
+);
