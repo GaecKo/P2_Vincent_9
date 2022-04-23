3b207d088c2a66c38bdbc1b14728dc22402273a6
Arthur De Neyer <arthur@arthur-linux>
2022-04-23 18:30:49 +0200

    first choseable graph

--- flaskr/main.py
+++ flaskr/main.py
@@ -2,7 +2,6 @@ from flask import Flask, redirect, url_for, render_template, request
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def to_home():
     return redirect(url_for("home"))
@@ -13,8 +12,7 @@ def home():
 
 @app.route("/analytics")
 def analytics():
-    liste  = ['Jean', 'Martine', 'Michel', 'Jules', 'Louise', 'Dominique']
-    return render_template('analytics.html', liste=liste)
+    return render_template('analytics.html')
 
 
 if __name__ == "__main__":
