0ff70122a9631310431e3219751c315533325cf9
Arthur - GaecKo <arthur.deneyer@student.uclouvain.be>
2022-04-30 17:55:22 +0200

    changed lot of things to make it workeable

--- flaskr/main.py
+++ flaskr/main.py
@@ -10,13 +10,23 @@ def to_home():
 def home():
     return render_template('home.html')
 
-@app.route("/analytics")
+@app.route("/graph", methods=['POST'])
+def graph():
+    start_time = request.form["start"]
+    end_time = request.form["end"]
+    famille = request.form["familles"]
+    # appel de la fonction avec la db
+    # récupération des données 
+    nom = f"{start_time} | {end_time} | {famille}"
+    graph = 'line' #"le type de graphe en fonction des choix données"
+    labeles = ["a", "b", "c"] #"donnés par la fonction"
+    datas = [10, 20, 30] #"donnés par la fonction"
+    if request.method == "POST":
+        return render_template('graph.html', type=graph, main_label = nom, labels=str(labeles), data=datas)
+
+@app.route("/analytics", methods=['GET', 'POST'])
 def analytics():
     return render_template('analytics.html')
-
-
-# def set_graph(type_chart, color, main_label):
-#     return (type_chart ,color, main_label, labels, data)
 
 
 if __name__ == "__main__":
