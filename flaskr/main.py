e3576db74ee2828b3480e9e1f9017e1f1dbe1b4e
Arthur De Neyer <arthur.deneyer@student.uclouvain.be>
2022-05-02 14:59:18 +0200

    added comment allover the code

--- flaskr/main.py
+++ flaskr/main.py
@@ -5,26 +5,26 @@ app = Flask(__name__)
 
 @app.route("/")
 def to_home():
-    return redirect(url_for("home"))
+    return redirect(url_for("home")) # redirection vers la page d'acceuil automatique 
 
 @app.route("/home")
 def home():
-    return render_template('home.html')
+    return render_template('home.html') # page d'acceuil
 
 @app.route("/graph", methods=['POST'])
 def graph():
     start_time = request.form["start"]
     end_time = request.form["end"]
-    famille = request.form["familles"]
+    famille = request.form["familles"] # récupération des informations
     graphe_to_show = request.form["graphe"]
-    labels, data, graph = get_infos(start_time, end_time, famille, graphe_to_show)
+    labels, data, graph = get_infos(start_time, end_time, famille, graphe_to_show) # envoit des infos vers des fonctions annexes qui s'en chargent
     somme = sum(data)
-    maximum = max(data)
+    maximum = max(data) # quelques stats des données récupérées pour afficher dans les paragraphes
     print(maximum)
     if request.method == "POST":
-        return render_template('graph.html', type=graph, main_label = graphe_to_show, labels=labels, data=data, somme=somme, max=maximum)
+        return render_template('graph.html', type=graph, main_label = graphe_to_show, labels=labels, data=data, somme=somme, max=maximum) # page de graphe
 
-@app.route("/analytics", methods=['GET', 'POST'])
+@app.route("/analytics", methods=['GET', 'POST']) # page de form pour récupérer les infos
 def analytics():
     return render_template('analytics.html')
 
