d393a03e20dc751ceba7188c13bfb9a706cd8ece
Arthur - GaecKo <arthur.deneyer@student.uclouvain.be>
2022-05-08 23:11:33 +0200

    added legend to graph when families influence. Still has to work on design (for Alex) and make sure no bugs

--- flaskr/main.py
+++ flaskr/main.py
@@ -17,11 +17,13 @@ def graph():
     start_time = request.form["start"] # récup des dates (start et end)
     end_time = request.form["end"]
     graphe_to_show = request.form["graphe"]
-    info_data["main_label"] = graphe_to_show
+    
 
 # paramêtres facultatifs
     try:
         famille = request.form.getlist("familles") # récupération des familles
+        info_data["famille_id"] = famille
+        
     except:
         famille = []
     try:
@@ -31,8 +33,9 @@ def graph():
         races = []
 
 # autres informations / créations d'informations depuis SQL
-
-    info_data["labels"], info_data["data"], info_data["graph"], info_data["background"] = get_infos(start_time, end_time, famille, graphe_to_show, races, pourcentage) # envoit des infos vers une fonction annexe qui s'en charge
+    info_data["main_label"] = graphe_to_show
+    info_data["famille_name"] = id_famille_to_name(famille)
+    info_data["labels"], info_data["data"], info_data["graph"], info_data["background"], info_data["color_familly"] = get_infos(start_time, end_time, famille, graphe_to_show, races, pourcentage) # envoit des infos vers une fonction annexe qui s'en charge
 
     if info_data["background"] in [[], "", None]:
         info_data["background"] = "'rgba(0, 240, 0, 1)'"
@@ -60,8 +63,10 @@ def graph():
 def analytics():
     return render_template('analytics.html', no_graph=False)
     
-    
+def id_famille_to_name(list_famille):
+    return [famillies[int(i)] for i in list_famille]
 
-
+global famillies
+famillies = {134: 'Margueritte', 45: 'Jonquille', 157: 'Lila', 99: 'Paquerette', 65: 'Agapanthe', 149: 'Bleuet', 61: 'Ciboulette', 141: 'Iris', 161: 'Lavande', 117: 'Lotus', 164: 'Hélène', 123: 'Milka', 16: "Côte d'Or", 77: 'Lindt', 139: 'Toblerone', 1: 'Galak', 92: 'KitKat', 116: 'Kinder', 30: 'Narcisse', 120: 'Orchidée', 47: 'Pissenlit', 96: 'Tournesol', 4: 'Vincent', 79: 'Trompette', 29: 'Minnie', 140: 'Rosette', 85: 'Blondie', 104: 'Caprice', 82: 'Betty', 97: 'Noisette', 10: 'Margot', 2: 'Réglisse', 15: 'Ruby', 126: 'Rustique', 144: 'Raymonde', 152: 'Sky', 110: 'Snow', 146: 'Sauterelle', 111: 'Star', 147: 'Sidonie', 50: 'Saturne', 17: 'Sorbet', 115: 'Sonette', 94: 'Summer', 63: 'Plume', 35: 'Peluche', 84: 'Pamela', 5: 'Panade', 69: 'Perle', 136: 'Papaye', 26: 'Renaud', 39: 'Papyrus', 22: 'Potache', 74: 'Pomme', 25: 'Pascaline', 48: 'Pink', 67: 'Provence', 148: 'Prune', 33: 'Paula', 18: 'Paupiette', 83: 'Pupuce', 21: 'Oasis', 80: 'Ocarina', 12: 'Orange', 91: 'Orage', 32: 'Origami', 20: 'Odile', 108: 'Ollande', 13: 'Olympe', 81: 'Oméga', 145: 'Otaria', 121: 'Okaly', 38: 'Onyx', 73: 'Olive', 106: 'Origan', 54: 'Ovalie', 75: 'Ophélie', 98: 'Ophia', 102: 'Ninette', 28: 'Ayham', 88: 'Ninon', 162: 'Naya', 138: 'Naza', 118: 'Nadine', 66: 'Nitendo', 154: 'Neda', 150: 'Noire', 129: 'Blanche', 142: 'Nairobi', 90: 'Nolvenn', 124: 'Nela', 70: 'Nora', 127: 'Fraise', 100: 'Banane', 72: 'Poire', 46: 'Citron', 119: 'Benoît', 128: 'Normande', 76: 'Norma', 135: 'Neptune', 27: 'Neslie', 40: 'Nancy', 8: 'Nostalgie', 156: 'Nouille', 109: 'Naomi', 11: 'Nourrice', 19: 'Nounou', 95: 'Nice', 9: 'Nubia', 23: 'Nash', 112: 'Numerobis', 114: 'Nuggets', 42: 'Nassma', 87: 'Nya', 163: 'Nilla', 159: 'Nathalia', 158: 'Nina', 36: 'Nima', 6: 'Rubarbe', 31: 'Maserati', 78: 'Mercedes', 43: 'Ferraris', 14: 'Ford', 86: 'Citroenne', 155: 'Jeep', 151: 'Magma', 137: 'Maite', 51: 'Madi', 62: 'Motorola', 24: 'Mirabelle', 59: 'Mazda', 153: 'Fiesta', 122: 'Malaisie', 34: 'Mamamia', 49: 'Météorite', 130: 'Moutarde', 113: 'Mosaic', 132: 'Moselle', 133: 'Mandala', 53: 'Meringue', 58: 'Micheline', 101: 'Midas', 107: 'Mimosa', 3: 'Myrtille', 103: 'Mystic', 41: 'Mushroom', 93: 'Moussaka', 52: 'Moon', 60: 'Mastar', 68: 'Céline', 143: 'Mina', 131: 'Macaroni', 44: 'Madona', 56: 'Majestic', 57: 'Bessie', 37: 'Clarabelle', 125: 'Penny', 105: 'Dottie', 160: 'Tonnerre', 55: 'Oreo', 64: 'Panda', 7: 'Bimbo', 71: 'Moka', 89: 'Mélasse', 500: 'Unknown'}
 if __name__ == "__main__":
     app.run(debug=True)
